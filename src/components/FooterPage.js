5c3f7a3a6af552495d3292605d3fa822ab28dfab
salvadorf8 <aztecsalvador@gmail.com>
2020-06-14 19:43:44 -0500

    SF - changed order of column in footer at responsive breakpoint

--- src/components/FooterPage.js
+++ src/components/FooterPage.js
@@ -7,10 +7,7 @@ const FooterPage = () => {
         <MDBFooter className='font-small  black-text footer '>
             <MDBContainer fluid className='text-center text-md-left'>
                 <MDBRow>
-                    <MDBCol md='7 ' className='action-left'>
-                        <img src='https://res.cloudinary.com/dtxzobw6b/image/upload/c_scale,w_693/v1592108700/dulce_antojos/cake_logo_0613_fc5to9.png' alt='' className='img-fluid ' />
-                    </MDBCol>
-                    <MDBCol md='5'>
+                    <MDBCol md='5' className='order-md-1'>
                         <MDBRow className='py-5 action-right'>
                             <MDBCol>
                                 <a href='https://www.facebook.com/dulceantojosdfw'>
@@ -38,6 +35,9 @@ const FooterPage = () => {
                                 </a>
                             </MDBCol>
                         </MDBRow>
+                    </MDBCol>
+                    <MDBCol md='7 ' className='action-left'>
+                        <img src='https://res.cloudinary.com/dtxzobw6b/image/upload/c_scale,w_693/v1592108700/dulce_antojos/cake_logo_0613_fc5to9.png' alt='' className='img-fluid ' />
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>

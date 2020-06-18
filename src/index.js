afe2ab2ca41879d9961641a3d5cafb352a9b2fd7
salvadorf8 <aztecsalvador@gmail.com>
2020-06-18 14:57:41 -0500

    SF - removed parallax code, was no longer used

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 import App from './components/App';
 
 ReactDOM.render(
-    <ParallaxProvider scrollAxis='vertical'>
-        <App />
-    </ParallaxProvider>,
+    <App />,
     document.querySelector('#root')
 );
